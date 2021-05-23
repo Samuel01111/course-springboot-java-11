2521455398f4ac981fd504450f8408edc21e3c0e
Samuel01111 <Samuel.nascimento.2015@hotmail.com>
2021-05-23 20:26:56 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/resources/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/ResourceExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.educandoweb.course.resources.exceptions.StandardError;
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResouceNotFoundException;
 
 @ControllerAdvice
@@ -19,6 +20,13 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResouceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+		String error = "DataBase error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
